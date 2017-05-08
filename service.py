313a749694edcc42b8706f6c4dd17b93a4952a6a
Ravi Yadav <ryadav88@gmail.com>
2017-05-07 19:36:05 -0700

    Update service.py

--- service.py
+++ service.py
@@ -199,4 +199,4 @@ if __name__ == '__main__':
     #ov_client = OneViewClient.from_environment_variables()
     #ov_client = OneViewClient(config)
     #app.run(debug=True)
-    app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
